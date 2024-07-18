0ceaa2470b075aeb34a0f60e7b70250049bd6eb3
winoz <winoz@naver.com>
2024-07-18 22:10:40 +0900

    crud-server#2

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "--------------- 서버 배포 시작 -----------------"
-cd /home/ubuntu/crud-server
+cd /home/ubuntu/2024devSpringV1
 sudo fuser -k -n tcp 8080 || true
 nohup java -jar project.war > /dev/null 2>&1 &
 echo "--------------- 서버 배포 끝 -----------------"
